4814c0cbbaac110f8fee30050d75f8c5da6ac5e7
zwei <zwei.lmao.mail@gmail.com>
2024-02-24 20:30:31 -0500

    a

--- src/App.js
+++ src/App.js
@@ -11,10 +11,10 @@ function App() {
     const aboutRef = React.createRef();
     const experiencesRef = React.createRef();
     const teamRef = React.createRef();
-    
+
     return (
         <div className="App">
-            <Topbar refList={{"about": aboutRef, "experiences": experiencesRef, "team": teamRef}}></Topbar>
+            <Topbar refList={{ "about": aboutRef, "experiences": experiencesRef, "team": teamRef }}></Topbar>
             <div className="scroll-content">
                 <Banner ref={aboutRef}></Banner>
                 <Experiences ref={experiencesRef} experiences={
@@ -32,101 +32,170 @@ function App() {
                         }
                     ]
                 }></Experiences>
-                <Team title="Our Team" desc="Everyone on this list is either actively helping out at Spawnpoint or part of our awesome Discord team!"
-                ref={teamRef} members={
-                    [
-                        {
-                            name: "Zwei",
-                            role: "Lead Gameplay Engineer",
-                            image: "https://avatars.githubusercontent.com/u/83478397?v=4",
-                            link: "https://www.roblox.com/users/193503404/profile"
-                        },
-                        {
-                            name: "Nissanette",
-                            role: "Lead 3D Artist",
-                            image: "https://i.imgur.com/IZEz0ff.png",
-                            link: "https://www.roblox.com/users/193503404/profile"
-                        },
-                        {
-                            name: "Polly",
-                            role: "Lead Project Manager & Marketing",
-                            image: "https://i.imgur.com/FKLij8A.png",
-                            link: "https://www.roblox.com/users/96565404/profile"
-                        },
-                        {
-                            name: "Fazey",
-                            role: "Founder, Music, Environments, Models",
-                            image: "https://i.imgur.com/iM4wkUn.png",
-                            link: "https://www.roblox.com/users/94162462/profile"
-                        },
-                        {
-                            name: "JkGb",
-                            role: "Lead Animator",
-                            image: "https://i.imgur.com/IYPQiyi.png",
-                            link: "https://www.roblox.com/users/128307136/profile"
-                        },
-                        {
-                            name: "Torre",
-                            role: "2D Artist & Graphics Designer",
-                            image: "https://i.imgur.com/X1NTKJr.png",
-                            link: "https://twitter.com/TorreTheFool"
-                        },
-                        {
-                            name: "Springy",
-                            role: "Voice Acting",
-                            image: "https://i.imgur.com/J1vgpkf.png",
-                            link: "https://twitter.com/SpringyTheC_S"
-                        },
-                        {
-                            name: "katfanf",
-                            role: "Marketing & Moderation",
-                            image: "https://i.imgur.com/YolqnqA.png",
-                            link: "https://twitter.com/katsterlingfan"
-                        },
-                        {
-                            name: "Ritoriam",
-                            role: "QA",
-                            image: "https://i.imgur.com/jCGcAuM.jpeg",
-                            link: "https://twitter.com/Ritoriam3D"
-                        },
-                        {
-                            name: "ToxZinn",
-                            role: "Lead Discord Moderation",
-                            image: "https://i.imgur.com/qlUdwgF.png",
-                            link: "https://twitter.com/ToxZinn"
-                        },
-                        {
-                            name: "Vintii",
-                            role: "QA",
-                            image: "https://i.imgur.com/CXap13i.png",
-                            link: ""
-                        },
-                        {
-                            name: "Fredder Fastbear",
-                            role: "QA",
-                            image: "https://i.imgur.com/BiordUD.png",
-                            link: "https://twitter.com/fredderfastbear"
-                        },
-                        {
-                            name: "ElementEarth",
-                            role: "Discord Moderation",
-                            image: "https://i.imgur.com/i37zrVI.jpeg",
-                            link: "https://x.com/elementearth100"
-                        },
-                        {
-                            name: "lolmansreturn",
-                            role: "Programming Helper",
-                            image: "https://i.imgur.com/VVcPfJG.png",
-                            link: ""
-                        },
-                        {
-                            name: "Khaled",
-                            role: "QA",
-                            image: "https://i.imgur.com/HQyRmWF.png",
-                            link: "https://twitter.com/DarkestSFMZ"
-                        }
-                    ]
-                }></Team>
+                <Team title="Developers" desc="People who actively develop our game."
+                    ref={teamRef} members={
+                        [
+                            {
+                                name: "Zwei",
+                                role: "Lead Gameplay Engineer",
+                                image: "https://avatars.githubusercontent.com/u/83478397?v=4",
+                                link: "https://www.roblox.com/users/193503404/profile"
+                            },
+                            {
+                                name: "Nissanette",
+                                role: "Lead 3D Artist",
+                                image: "https://i.imgur.com/IZEz0ff.png",
+                                link: "https://www.roblox.com/users/193503404/profile"
+                            },
+                            {
+                                name: "Polly",
+                                role: "Lead Project Manager & Marketing",
+                                image: "https://i.imgur.com/FKLij8A.png",
+                                link: "https://www.roblox.com/users/96565404/profile"
+                            },
+                            {
+                                name: "Fazey",
+                                role: "Founder, Music, Environments, Models",
+                                image: "https://i.imgur.com/iM4wkUn.png",
+                                link: "https://www.roblox.com/users/94162462/profile"
+                            },
+                            {
+                                name: "JkGb",
+                                role: "Lead Animator",
+                                image: "https://i.imgur.com/IYPQiyi.png",
+                                link: "https://www.roblox.com/users/128307136/profile"
+                            },
+                            {
+                                name: "Torre",
+                                role: "2D Artist & Graphics Designer",
+                                image: "https://i.imgur.com/X1NTKJr.png",
+                                link: "https://twitter.com/TorreTheFool"
+                            },
+                            {
+                                name: "Springy",
+                                role: "Voice Acting",
+                                image: "https://i.imgur.com/J1vgpkf.png",
+                                link: "https://twitter.com/SpringyTheC_S"
+                            },
+                            {
+                                name: "katfanf",
+                                role: "Marketing & Moderation",
+                                image: "https://i.imgur.com/YolqnqA.png",
+                                link: "https://twitter.com/katsterlingfan"
+                            },
+                            {
+                                name: "Ritoriam",
+                                role: "QA",
+                                image: "https://i.imgur.com/jCGcAuM.jpeg",
+                                link: "https://twitter.com/Ritoriam3D"
+                            },
+                            {
+                                name: "ToxZinn",
+                                role: "Lead Discord Moderation",
+                                image: "https://i.imgur.com/qlUdwgF.png",
+                                link: "https://twitter.com/ToxZinn"
+                            },
+                            {
+                                name: "Vintii",
+                                role: "QA",
+                                image: "https://i.imgur.com/CXap13i.png",
+                                link: ""
+                            },
+                            {
+                                name: "Fredder Fastbear",
+                                role: "QA",
+                                image: "https://i.imgur.com/BiordUD.png",
+                                link: "https://twitter.com/fredderfastbear"
+                            },
+                            {
+                                name: "ElementEarth",
+                                role: "Discord Moderation",
+                                image: "https://i.imgur.com/i37zrVI.jpeg",
+                                link: "https://x.com/elementearth100"
+                            },
+                            {
+                                name: "lolmansreturn",
+                                role: "Programming Helper",
+                                image: "https://i.imgur.com/VVcPfJG.png",
+                                link: ""
+                            },
+                            {
+                                name: "Khaled",
+                                role: "QA",
+                                image: "https://i.imgur.com/HQyRmWF.png",
+                                link: "https://twitter.com/DarkestSFMZ"
+                            }
+                        ]
+                    }></Team>
+                <Team title="Contributors" desc="People who contribute to the game occasionally."
+                    ref={teamRef} members={
+                        [
+                            {
+                                name: "Springy",
+                                role: "Voice Acting",
+                                image: "https://i.imgur.com/J1vgpkf.png",
+                                link: "https://twitter.com/SpringyTheC_S"
+                            },
+                            {
+                                name: "katfanf",
+                                role: "Marketing & Moderation",
+                                image: "https://i.imgur.com/YolqnqA.png",
+                                link: "https://twitter.com/katsterlingfan"
+                            },
+                            {
+                                name: "lolmansreturn",
+                                role: "Programming Helper",
+                                image: "https://i.imgur.com/VVcPfJG.png",
+                                link: ""
+                            }
+                        ]
+                    }></Team>
+                <Team title="Discord Staff" desc="Our awesome Discord team!"
+                    ref={teamRef} members={
+                        [
+                            {
+                                name: "ToxZinn",
+                                role: "Lead Discord Moderation",
+                                image: "https://i.imgur.com/qlUdwgF.png",
+                                link: "https://twitter.com/ToxZinn"
+                            },
+                            {
+                                name: "ElementEarth",
+                                role: "Discord Moderation",
+                                image: "https://i.imgur.com/i37zrVI.jpeg",
+                                link: "https://x.com/elementearth100"
+                            }
+                        ]
+                    }></Team>
+                <Team title="QA" desc="Our amazing QA team!"
+                    ref={teamRef} members={
+                        [
+                            {
+                                name: "Ritoriam",
+                                role: "QA",
+                                image: "https://i.imgur.com/jCGcAuM.jpeg",
+                                link: "https://twitter.com/Ritoriam3D"
+                            },
+                            {
+                                name: "Vintii",
+                                role: "QA",
+                                image: "https://i.imgur.com/CXap13i.png",
+                                link: ""
+                            },
+                            {
+                                name: "Fredder Fastbear",
+                                role: "QA",
+                                image: "https://i.imgur.com/BiordUD.png",
+                                link: "https://twitter.com/fredderfastbear"
+                            },
+                            {
+                                name: "Khaled",
+                                role: "QA",
+                                image: "https://i.imgur.com/HQyRmWF.png",
+                                link: "https://twitter.com/DarkestSFMZ"
+                            }
+                        ]
+                    }></Team>
             </div>
         </div>
     );
